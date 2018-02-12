0481850ed08312c1a6ac378c835e7d5e0206a68c
Karyakina <Worc_ki@list.ru>
2018-02-12 09:59:14 +0300

    добавлена функция

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        double a=6;
+        double b=10;
+        System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" равна "+area(a, b));
+    }
+    public static double area(double a, double b) {
+       return a*b;
     }
 }
